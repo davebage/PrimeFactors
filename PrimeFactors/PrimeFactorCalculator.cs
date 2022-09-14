94ee9026585ee05946f31814fdec246eccb4891a
Dave Bage <david_bage@hotmail.com>
2022-09-14 13:42:06 +0100

    Added test for non positive exception being thrown and for testing value for 3

--- PrimeFactors/PrimeFactorCalculator.cs
+++ PrimeFactors/PrimeFactorCalculator.cs
@@ -4,6 +4,8 @@
     {
         public int GeneratePrimeFactorsFor(int iNumber)
         {
+            if(iNumber < 0) throw new ArgumentOutOfRangeException(nameof(iNumber));
+            if (iNumber == 3) return 3;
             return 2;
         }
     }
--- Prime_Factor_Calculator_Tests/Prime_Factor_Calculator_Should.cs
+++ Prime_Factor_Calculator_Tests/Prime_Factor_Calculator_Should.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using PrimeFactors;
 
@@ -11,11 +12,27 @@ namespace Prime_Factor_Calculator_Tests
         }
 
         [Test]
+        public void throw_exception_for_non_positive_input()
+        {
+            PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => primeFactorCalculator.GeneratePrimeFactorsFor(-1));
+        }
+
+        [Test]
         public void return_two_for_input_of_two()
         {
             PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
 
             Assert.That(primeFactorCalculator.GeneratePrimeFactorsFor(2), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void return_three_for_input_of_three()
+        {
+            PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
+
+            Assert.That(primeFactorCalculator.GeneratePrimeFactorsFor(3), Is.EqualTo(3));
         }
     }
 }
