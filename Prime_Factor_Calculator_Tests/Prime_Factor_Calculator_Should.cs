87f421fa99fffaa2e322247a6774ac710a41c73a
Dave Bage <david_bage@hotmail.com>
2022-09-14 13:45:29 +0100

    Refactored tests to instatiate PrimeFactorCalculator in setup

--- Prime_Factor_Calculator_Tests/Prime_Factor_Calculator_Should.cs
+++ Prime_Factor_Calculator_Tests/Prime_Factor_Calculator_Should.cs
@@ -6,32 +6,29 @@ namespace Prime_Factor_Calculator_Tests
 {
     public class Prime_Factor_Calculator_Should
     {
+        private PrimeFactorCalculator primeFactorCalculator;
+
         [SetUp]
         public void Setup()
         {
+            primeFactorCalculator = new PrimeFactorCalculator();
         }
 
         [Test]
         public void throw_exception_for_non_positive_input()
         {
-            PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
-
             Assert.Throws<ArgumentOutOfRangeException>(() => primeFactorCalculator.GeneratePrimeFactorsFor(-1));
         }
 
         [Test]
         public void return_two_for_input_of_two()
         {
-            PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
-
             Assert.That(primeFactorCalculator.GeneratePrimeFactorsFor(2), Is.EqualTo(2));
         }
 
         [Test]
         public void return_three_for_input_of_three()
         {
-            PrimeFactorCalculator primeFactorCalculator = new PrimeFactorCalculator();
-
             Assert.That(primeFactorCalculator.GeneratePrimeFactorsFor(3), Is.EqualTo(3));
         }
     }
